aff9c8e635f67a08037d650c56b0160358a6e3e3
Nicolas Cardozo <ncardozo@vub.ac.be>
2014-02-14 16:05:55 +0100

    View of the matches for a group
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

--- Client/Mobile Polla/Resources/ui/handheld/ApplicationWindow.js
+++ Client/Mobile Polla/Resources/ui/handheld/ApplicationWindow.js
@@ -1,6 +1,7 @@
 //var network = require('Network');
 var network = require('Network/Network');
 
+
 function ApplicationWindow(title) {
 	var self = Ti.UI.createWindow({
 		title:title,
@@ -10,7 +11,7 @@ function ApplicationWindow(title) {
 	var button = Ti.UI.createButton({
 		height:44,
 		width:200,
-		title:L('openWindow'),
+		title:L('to match center'),
 		top:20
 	});
 	self.add(button);
@@ -18,55 +19,13 @@ function ApplicationWindow(title) {
 	button.addEventListener('click', function() {
 		//containingTab attribute must be set by parent tab group on
 		//the window for this work
-		
-		network.getTeams({name: "BOBY!!"});
 
 		var BetWindow = require('/ui/handheld/BetsWindow');
     	var myBetWindow = BetWindow.BetsWindow();
     
 		self.containingTab.open(myBetWindow);
 		
-		
-		
 	});
-	
-	
-	// create table view data object
-var data = [];
- 
-for (var c=0;c<4;c++)
-{
-    data[c] = Ti.UI.createTableViewSection({headerTitle:'Group '+(c+1)});
-    for (var x=0;x<10;x++)
-    {
-        data[c].add(Ti.UI.createTableViewRow({title:'Group '+(c+1)+', Row '+(x+1)}));
-    }
-}
- 
-// create table view
-var tableview = Titanium.UI.createTableView({
-    data:data,
-    style: Titanium.UI.iPhone.TableViewStyle.GROUPED
-});
- 
-// create table view event listener
-tableview.addEventListener('click', function(e)
-{
-    // event data
-    var index = e.index;
-    var section = e.section;
-    var row = e.row;
-    var rowdata = e.rowData;
-    if (section.headerTitle.indexOf('clicked')==-1)
-    {
-        section.headerTitle = section.headerTitle + ' (clicked)';
-    }
-    Titanium.UI.createAlertDialog({title:'Table View',message:'row ' + row + ' index ' + index + ' section ' + section  + ' row data ' + rowdata}).show();
-});
- 
-// add table view to the window
-//self.add(tableview);
-
 
 
 	return self;
---
+++ Client/Mobile Polla/Resources/ui/handheld/MatchListWindow.js
@@ -0,0 +1,137 @@
+var network = require('Network/Network');
+
+function MatchesWindow() {
+	var self = Ti.UI.createWindow({
+		title : L('Matches for Group 1'),
+		backgroundColor : 'white'
+	});
+
+	// create table view data object
+	var data = [];
+
+	for (var c = 0; c < 5; c++) {
+		data[c] = Ti.UI.createTableViewSection({
+			headerTitle : '  ',
+			height : 30
+		});
+		var row1 = Ti.UI.createTableViewRow({
+			className : 'row',
+			objName : 'row',
+			touchEnabled : true,
+			height : 50
+		});
+		row1.add(Titanium.UI.createImageView({
+			image : "/images/COL.png",
+			left : 10,
+			height : 20,
+			width : 30
+		}));
+		row1.add(Titanium.UI.createLabel({
+			text : 'COL',
+			top : 13,
+			left : 50
+		}));
+		row1.add(Titanium.UI.createImageView({
+			image : "/images/BEL.png",
+			left : 220,
+			height : 20,
+			width : 30
+		}));
+		row1.add(Titanium.UI.createLabel({
+			text : 'BEL',
+			top : 13,
+			left : 260
+		}));
+		data[c].add(row1);
+		var row2 = Ti.UI.createTableViewRow({
+			className : 'row',
+			objName : 'row',
+			touchEnabled : true,
+			height : 100
+		});
+
+		var picker = Titanium.UI.createPicker({
+			left : 10,
+			width : 40,
+			heigh : 50
+		});
+		var data2 = [];
+		var data3 = [];
+		data2.push(Titanium.UI.createPickerRow({
+			title : ' '
+		}));
+		data3.push(Titanium.UI.createPickerRow({
+			title : ' '
+		}));
+		for (var j = 1; j < 11; j++) {
+			data2.push(Titanium.UI.createPickerRow({
+				title : j.toString()
+			}));
+			data3.push(Titanium.UI.createPickerRow({
+				title : j.toString()
+			}));
+		}
+		var picker2 = Titanium.UI.createPicker({
+			left : 220,
+			width : 40,
+			heigh : 50
+		});
+
+		picker.add(data2);
+		picker2.add(data3);
+		row2.add(picker);
+		row2.add(picker2);
+		data[c].add(row2);
+	}
+
+	data[7] = Ti.UI.createTableViewSection({
+		headerTitle : '  ',
+		height : 40
+	});
+	var buttonRow = Ti.UI.createTableViewRow({
+		className : 'row',
+		objName : 'row',
+		touchEnabled : true,
+		height : 50
+	});
+	var rightButton = Titanium.UI.createButton({
+			title: 'OK',
+			right : 10,
+		});
+	rightButton.addEventListener('click', function(e) {
+			Ti.API.info('clicked button');
+			
+	}); 
+	
+	buttonRow.add(rightButton);
+	data[7].add(buttonRow);
+
+	// create table view
+	var tableview = Titanium.UI.createTableView({
+		data : data,
+		style : Titanium.UI.iPhone.TableViewStyle.GROUPED
+	});
+
+	// create table view event listener
+	/*	tableview.addEventListener('click', function(e) {
+	// event data
+	var index = e.index;
+	var section = e.section;
+	var row = e.row;
+	var rowdata = e.rowData;
+	if (section.headerTitle.indexOf('clicked') == -1) {
+	section.headerTitle = section.headerTitle + ' (clicked)';
+	}
+	Titanium.UI.createAlertDialog({
+	title : 'Table View',
+	message : 'row ' + row + ' index ' + index + ' section ' + section + ' row data ' + rowdata
+	}).show();
+	});
+	*/
+	// add table view to the window
+	self.add(tableview);
+
+	return self;
+};
+
+module.exports.MatchesWindow = MatchesWindow;
