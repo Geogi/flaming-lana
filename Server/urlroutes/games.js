c20daea43d20f0a945e6be9382de47293e2a3ac3
alombide <alombide@vub.ac.be>
2014-02-14 11:15:42 +0100

    Adding empty object module setup

---
+++ Server/urlroutes/games.js
@@ -0,0 +1,15 @@
+/*
+ * A round object looks as follows:
+ *
+ *  {
+ *      team1_key,
+ 		team2_key,
+ 		team1_title,
+ 		team2_title,
+ 		team1_code,
+ 		team2_code,
+ 		play_at,
+ 		score_1,
+ 		score_2
+ *  }
+ */
