73568dac3aa07183ce2ae18b63aaa89429fcaab1
Christophe Scholliers <cfscholl@vub.ac.be>
2014-02-14 16:49:04 +0100

    tesst

--- Client/Mobile Polla/Resources/ui/common/ApplicationTabGroup.js
+++ Client/Mobile Polla/Resources/ui/common/ApplicationTabGroup.js
@@ -10,27 +10,11 @@ function ApplicationTabGroup(Window) {
 		myBetWindow = new Window(L('bet')),
 		win3 = new Window(L('money'));
 
-	
-    // pull data to display
-	 network.getGroups(function(response) {
-		 var BetWindow = require('/ui/handheld/BetsWindow');
-		 var groupData = [];
-		 for (var c = 0; c < response.length; c++) {
-			 var group = response[c];
-			 var betSize = group.games.length;
-			 if (betSize <= 0) { betSize = 6; };
-				 groupData.push([group.name, betSize]);
-		 };
-		// this.tab2.window = BetWindow.BetsWindow(groupData);		
-		 
-	 });
-	//var BetWindow = require('/ui/handheld/BetsWindow');
-	//myBetWindow = BetWindow.BetsWindow([['Group A', 5], ['Group B', 6], ['Group C', 7]]);	
-    
-    
-    
-    var standingWindow = require('/ui/handheld/StandingWindow');
+	var standingWindow = require('/ui/handheld/StandingWindow');
     var myStandingWindow = standingWindow.StandingWindow();
+    
+  
+
 
 	var tab1 = Ti.UI.createTab({
 		title: L('standings'),
