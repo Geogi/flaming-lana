a1729e0e9ddc2ec37c7636cc0480cbb28d4f1161
Elisa Gonzalez Boix <egonzale@vub.ac.be>
2014-02-14 14:22:17 +0100

    displaying group in view

--- Client/Mobile Polla/Resources/ui/handheld/BetsWindow.js
+++ Client/Mobile Polla/Resources/ui/handheld/BetsWindow.js
@@ -1,51 +1,107 @@
 var network = require('Network/Network');
 
+// items of the form [ [groupName, betSize], ...]
+function BetsWindow(items) {
 
-function BetsWindow() {
 	var self = Ti.UI.createWindow({
-		title:"marrano",
-		backgroundColor:'white'
+		title : "Bets",
+		backgroundColor : 'white'
 	});
-		
-// create table view data object
-var data = [];
- 
-for (var c=0;c<4;c++)
-{
-    data[c] = Ti.UI.createTableViewSection({headerTitle:'Group '+(c+1)});
-    for (var x=0;x<10;x++)
-    {
-        data[c].add(Ti.UI.createTableViewRow({title:'Group '+(c+1)+', Row '+(x+1)}));
-    }
+
+	// create empty array to add table data to
+	var data = [];
+
+	//create table view data object
+	for (var c = 0; c < items.length; c++) {
+		var group = items[c];
+		var titleR = group[0];
+		var betSize = group[1];
+		var itemID = c;
+		var progressBar = 'l' + c;
+
+		Ti.API.info(progressBar);
+
+		var progressBar = Titanium.UI.createProgressBar({
+			height : 50,
+			right: 15,
+			min : 0,
+			max : 1,
+			value : 0,
+			style : Titanium.UI.iPhone.ProgressBarStyle.PLAIN,
+			message : 'bets on ' + betSize + ' matches',
+			font : {
+				fontSize : 12,
+				fontWeight : 'bold'
+			},
+			color : '#888'
+		});
+
+		var row = Ti.UI.createTableViewRow({
+			height : 40,
+			backgroundColor : '#fff'
+		});
+
+		var rowTitle = Ti.UI.createLabel({
+			text : titleR,
+			color : '#666666',
+			left : 15,
+			font : {
+				fontSize : 13
+			},
+			height : 40,
+		});
+
+		var nestedView = Ti.UI.createView({
+			height : 40,
+		});
+
+		progressBar.show();
+		nestedView.add(progressBar);
+		row.add(rowTitle);
+		row.add(nestedView);
+		data.push(row);
+
 }
- 
+
 // create table view
 var tableview = Titanium.UI.createTableView({
-    data:data,
-    style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+	data : data,
+	style : Titanium.UI.iPhone.TableViewStyle.PLAIN
 });
- 
+
 // create table view event listener
-tableview.addEventListener('click', function(e)
-{
-    // event data
-    var index = e.index;
-    var section = e.section;
-    var row = e.row;
-    var rowdata = e.rowData;
-    if (section.headerTitle.indexOf('clicked')==-1)
-    {
-        section.headerTitle = section.headerTitle + ' (clicked)';
-    }
-    Titanium.UI.createAlertDialog({title:'Table View',message:'row ' + row + ' index ' + index + ' section ' + section  + ' row data ' + rowdata}).show();
+tableview.addEventListener('click', function(e) {
+	// event data
+	var index = e.index;
+	//var section = e.section;
+	var row = e.row;
+	
+	// if (section.rowdata.indexOf('clicked') == -1) {
+		// section.rowdata = section.rowdata + ' (clicked)';
+	// }
+	 Titanium.UI.createAlertDialog({
+		title : 'Table View',
+		message : 'row ' + row + ' index ' + index 
+	}).show();
 });
- 
+
+var buttonBet = Ti.UI.createButton({
+		height:44,
+		width:200,
+		title:L('Bet now'),
+		bottom:20
+});
+
+buttonBet.addEventListener('click', function() {
+
+		
+				
+});	
 // add table view to the window
-//self.add(tableview);
+self.add(tableview);
+self.add(buttonBet);
 
-
-
-	return self;
+return self;
 };
 
 module.exports.BetsWindow = BetsWindow;
