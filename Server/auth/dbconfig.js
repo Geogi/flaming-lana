3303fe91c2511eee56f1a8ccb73876feee627dfe
alombide <alombide@vub.ac.be>
2014-02-14 11:09:26 +0100

    Adding empty object module setup

--- Server/auth/dbconfig.js
+++ Server/auth/dbconfig.js
@@ -1,8 +1,10 @@
 ﻿var dbname = "MobilePolla";
 
 // Define collection names here...
-var teamsCollection = "teams";
-var groupsCollection = "groups";
+var teamsCollection = "mobilepolla_teams";
+var groupsCollection = "mobilepolla_groups";
+var roundsCollection = "mobilepolla_rounds";
+var gamesCollection = "mobilepolla_games";
 
 
 // In case of local MongoDB, use the default port for a MongoDB. Change this if your config is different.
@@ -12,3 +14,7 @@ var secret = "xxxxxxxx";
 
 
 exports.dbname = dbname;
+exports.teamsCollection = teamsCollection;
+exports.groupsCollection = groupsCollection;
+exports.roundsCollection = roundsCollection;
+exports.gamesCollection = gamesCollection;
---
+++ Server/urlroutes/teams.js
@@ -0,0 +1,9 @@
+/*
+ * A team object looks as follows:
+ *
+ *  {
+ *      name:     	Name of the team,
+        games:    	Games of the team,
+        key:  		Short name of the team
+ *  }
+ */
