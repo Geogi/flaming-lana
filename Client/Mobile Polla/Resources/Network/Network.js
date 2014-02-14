1d409ed728bf2ac47e5e73505212116ba4be2d98
Christophe Scholliers <cfscholl@vub.ac.be>
2014-02-14 10:23:54 +0100

    Added some small stuff for the network

---
+++ Client/Mobile Polla/Resources/Network/Network.js
@@ -0,0 +1,25 @@
+ var base_url = "http://10.221.54.225:8888";
+ var getTeamsUrl = base_url + "/groups/getgroup/";
+ 
+ 
+ var client = Ti.Network.createHTTPClient({
+     // function called when the response data is available
+     onload : function(e) {
+         Ti.API.info("Received text: " + this.responseText);
+         alert(this.responseText);
+     },
+     // function called when an error occurs, including a timeout
+     onerror : function(e) {
+         Ti.API.debug(e.error);
+         alert('error');
+     },
+     timeout : 5000  // in milliseconds
+ });
+ 
+ function getTeams(jobject) {
+ 	 // Prepare the connection.
+ 		client.open("POST", getTeamsUrl);
+ 		client.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+ 		// Send the request.
+ 		client.send(jobject);
+ }
